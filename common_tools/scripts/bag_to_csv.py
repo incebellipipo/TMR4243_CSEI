123548cfc58d84af0c3823002b1e48b2e7bd45b6
Emir Cem Gezer <emircem.gezer@gmail.com>
2023-03-27 11:20:00 +0200

    fix: argparser output

--- common_tools/scripts/bag_to_csv.py
+++ common_tools/scripts/bag_to_csv.py
@@ -142,7 +142,7 @@ if __name__ == '__main__':
     parser.add_argument("-n", "--no-header", dest="header",
                       action="store_false", default=True,
                       help="no header / flatten array value")
-    (options, args) = parser.parse_args()
+    args = parser.parse_args()
 
-    main(options)
+    main(args)
 
