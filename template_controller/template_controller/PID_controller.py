bd7800b18f72a2114e79766d3c371719fc78becd
Petter Hangerhagen <petter@hangerhagen.com>
2024-02-15 07:56:26 +0100

    first commit

---
+++ template_controller/template_controller/PID_controller.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+def PID_controller(observer, reference, P_gain, I_gain, D_gain):
+    
+    # Getting the states from the observer
+    eta_hat = observer.eta
+    nu_hat = observer.nu
+    bias_hat = observer.bias
+
+    # Getting the states from the refernce
+    eta_d = reference.eta_d
+    eta_ds = reference.eta_ds
+    eta_ds2 = reference.eta_ds2
+    w = reference.w
+    v_s = reference.v_s
+    v_ss = reference.v_ss
+
+    # Replace the following line
+    tau = [0, 0, 0]
+
+    #
+    ## Write your code below
+    #
+
+    return tau
