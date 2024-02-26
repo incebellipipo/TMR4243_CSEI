c0dc78d84a01e2612bd3c377eb35adcacb6c35f6
Emir Cem Gezer <emircem.gezer@gmail.com>
2024-02-26 15:52:13 +0100

    feat: fix controller

--- template_joystick_control/template_joystick_control/joystick_control_node.py
+++ template_joystick_control/template_joystick_control/joystick_control_node.py
@@ -25,7 +25,7 @@ import numpy as np
 
 import rclpy
 import rclpy.node
-from std_msgs.msg import Float64MultiArray
+import std_msgs.msg
 import geometry_msgs.msg
 import sensor_msgs.msg
 
@@ -33,6 +33,7 @@ from tf2_ros import TransformException
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 
+import template_joystick_control.joystick_mapping
 from template_joystick_control.joystick_simple import joystick_simple
 from template_joystick_control.joystick_force_basin_relative import joystick_force_basin_relative
 from template_joystick_control.joystick_force_body_relative import joystick_force_body_relative
@@ -54,13 +55,15 @@ class JoystickForce(rclpy.node.Node):
             geometry_msgs.msg.Wrench, '/CSEI/thrusters/port/command', 1)
         self.pubs["starboard"] = self.create_publisher(
             geometry_msgs.msg.Wrench, '/CSEI/thrusters/starboard/command', 1)
+        self.pubs["tau"] = self.create_publisher(
+            std_msgs.msg.Float32MultiArray, '/CSEI/control/tau', 1)
 
         self.subs["joy"] = self.create_subscription(
             sensor_msgs.msg.Joy, '/joy', self.joy_callback, 10)
 
         self.joystick_name = self.declare_parameter('joystick_name', 'PS4')
 
-        self.current_task  = self.declare_parameter('task', 'simple')
+        self.current_task = self.declare_parameter('task', 'simple')
         self.current_task.value
 
         self.last_transform = None
@@ -79,8 +82,8 @@ class JoystickForce(rclpy.node.Node):
 
         self.current_task = self.get_parameter('task')
 
-        self.get_logger().info(f"Parameter task: {self.current_task.value}", throttle_duration_sec=1.0)
-
+        self.get_logger().info(
+            f"Parameter task: {self.current_task.value}", throttle_duration_sec=1.0)
 
     def joy_callback(self, msg):
 
@@ -94,10 +97,10 @@ class JoystickForce(rclpy.node.Node):
             result = joystick_force_body_relative(msg)
 
         if len(result) != 5:
-            self.get_logger().warn(f"Result has length of {len(result)} but it should be 5", throttle_duration_sec=1.0)
+            self.get_logger().warn(
+                f"Result has length of {len(result)} but it should be 5", throttle_duration_sec=1.0)
 
         [u0, u1, u2, a1, a2] = result
-
 
         f = geometry_msgs.msg.Wrench()
         f.force.x = u0
@@ -113,6 +116,12 @@ class JoystickForce(rclpy.node.Node):
         f.force.y = u2 * math.sin(a2)
         self.pubs['starboard'].publish(f)
 
+        tau = std_msgs.msg.Float32MultiArray()
+        tau.data = list(msg.axes[template_joystick_control.joystick_mapping.LEFT_STICK_VERTICAL],
+                        msg.axes[template_joystick_control.joystick_mapping.LEFT_STICK_HORIZONTAL],
+                        msg.axes[template_joystick_control.joystick_mapping.RIGHT_STICK_VERTICAL])
+
+        self.pubs['tau'].publish(tau)
 
 
 def main(args=None):
