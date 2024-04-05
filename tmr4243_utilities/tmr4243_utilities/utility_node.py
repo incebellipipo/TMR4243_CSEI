038e8f6b2e16db00a7207596bd269af161340e4a
Emir Cem Gezer <emircem.gezer@gmail.com>
2024-04-05 09:54:34 +0200

    fix: eta data

--- tmr4243_utilities/tmr4243_utilities/utility_node.py
+++ tmr4243_utilities/tmr4243_utilities/utility_node.py
@@ -82,8 +82,8 @@ class UtilityNode(rclpy.node.Node):
 
         try:
             self.last_transform = self.tf_buffer.lookup_transform(
-                "base_link",
                 "world",
+                "base_link",
                 rclpy.time.Time())
             eta = [None] * 3
 
